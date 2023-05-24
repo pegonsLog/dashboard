0f45fa3bccc6020dffc722de2ab9fd056168eb3b
pegonsLog <pegons.log@gmail.com>
2023-05-24 17:28:55 -0300

    24/05/2023 - Trabalho

--- src/app/day/day-list/day-list.component.ts
+++ src/app/day/day-list/day-list.component.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { DayService } from '../day.service';
+import { Observable, Subscription } from 'rxjs';
+import { Certificate } from 'src/app/shared/models/Certificate';
 
 @Component({
   selector: 'app-day-list',
   templateUrl: './day-list.component.html',
-  styleUrls: ['./day-list.component.scss']
+  styleUrls: ['./day-list.component.scss'],
 })
-export class DayListComponent {
-
-  constructor(){
-    
-  }
+export class DayListComponent implements OnDestroy {
+  subscription: Subscription = new Subscription();
+  dataSource = [];
   displayedColumns: string[] = ['registration', 'startDay', 'endDay', 'mode'];
-  dataSource = new MatTableDataSource([
-    { id: '1', registration: '564', startDay: '22/05/2023', endDay: '25/05/2023', mode: 'Comparecimento' },
-    { id: '2', registration: '410', startDay: '23/05/2023', endDay: '29/05/2023', mode: 'Acompanhamento'}
-  ]);
 
-  applyFilter(event: KeyboardEvent) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  constructor(private dayService: DayService) {
+    this.subscription = dayService.list()
+      .subscribe(
+        (certificates: any) => (this.dataSource = certificates)
+      );
+  }
+
+
+
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/day/day.service.ts
+++ src/app/day/day.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { User } from '../shared/models/User';
+import { DocumentData, collection, getDocs, getFirestore } from 'firebase/firestore/lite';
+import { initializeApp } from 'firebase/app';
+import { environment } from 'src/environments/environment';
+import { Certificate } from '../shared/models/Certificate';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DayService {
 
+  app = initializeApp(environment.firebase);
+  db = getFirestore(this.app);
+
+  certificates: Certificate[] = [];
+  certificate: Certificate = {
+    id: '',
+    registration: '',
+    startDay: new Date,
+    endDay: new Date,
+    startHour: new Date,
+    endHour: new Date,
+    dayOff: new Date,
+    type: '',
+    mode: '',
+  };
+
   constructor() { }
+
+  list(): Observable<Certificate[]> {
+    const certificates = collection(this.db, 'certificate');
+    return new Observable<DocumentData[]>((subscriber) => {
+      getDocs(certificates)
+        .then((certificatesSnapshot) => {
+          const certificatesList = certificatesSnapshot.docs.map((doc) => doc.data());
+          subscriber.next(certificatesList);
+          subscriber.complete();
+        })
+        .catch((error) => {
+          subscriber.error(error);
+        });
+    }).pipe(map((certificatesList) => certificatesList as Certificate[]));
+  }
 }
